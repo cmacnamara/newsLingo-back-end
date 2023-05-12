03bbb029912fac8ab769bf263c9a4159d3233e07
Chris MacNamara <cpmacnamara@gmail.com>
2023-05-12 17:08:46 -0400

    add article route and controller files

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ import './config/database.js'
 // import routes
 import { router as profilesRouter } from './routes/profiles.js'
 import { router as authRouter } from './routes/auth.js'
+import { router as articlesRouter } from './routes/articles.js'
 
 // create the express app
 const app = express()
@@ -24,6 +25,7 @@ app.use(formData.parse())
 // mount imported routes
 app.use('/api/profiles', profilesRouter)
 app.use('/api/auth', authRouter)
+app.use('/api/articles', articlesRouter)
 
 // handle 404 errors
 app.use(function (req, res, next) {
