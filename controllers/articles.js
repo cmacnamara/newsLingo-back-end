5a53fab84f80e65eba758f1f980765ab04c77448
Daniel Mathews <daniel.g.mathews@gmail.com>
2023-05-12 22:41:43 -0400

    add articles index ccontroller function

--- controllers/articles.js
+++ controllers/articles.js
@@ -5,20 +5,32 @@ async function create(req,res) {
   try {
     const apiResponse = await fetch(`https://newsdata.io/api/1/news?apikey=${process.env.NEWS_API_KEY}&language=en`)
     const articleData = await apiResponse.json()
-    // console.log(articleData);
     const articles = await Article.create(articleData.results.filter((article, idx) => idx < 10))
     console.log(articles);
-    res.json(articleData)
+    res.status(200).json(articles)
   } catch (error) {
     console.log(error);
+    res.json(error)
   }
 }
 
 async function index(req,res) {
   try {
-    
-  } catch (error) {
-    console.log(error);
+    //first check to see if any articles exist that were created with recent mongodb timestamp
+    const sixteenHoursAgo = new Date()
+    sixteenHoursAgo.setHours(sixteenHoursAgo.getHours() - 16)
+  
+    const articles = await Article.find({ createdAt: { $gte: sixteenHoursAgo} })
+      articles.length >= 10
+      //if yes, 
+        /// are there at least 100 (10 for testing)?  if yes, index
+        ? res.status(200).json(articles)
+        //if no call create
+        : create(req, res)
+      } 
+  catch (error) {
+    console.log(error)
+    res.status(500).json(error)
   }
 }
 
