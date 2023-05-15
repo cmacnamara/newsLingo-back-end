33e78495d8fb04254b53797d9dc4c9495f1dbbb4
Eunice Yang <euniceyang21@gmail.com>
2023-05-15 13:11:51 -0500

    Build index dictionary

--- routes/words.js
+++ routes/words.js
@@ -13,9 +13,9 @@ router.use(decodeUserFromToken)
 
 //localhost:3001/api/words
 router.post('/', checkAuth, wordsCtrl.create)
-router.get('/', checkAuth, wordsCtrl.index)  
 router.get('/:wordId', checkAuth, wordsCtrl.show)
 router.delete('/:wordId', checkAuth, wordsCtrl.delete)
+router.get('/:userId/dictionary', checkAuth, wordsCtrl.index)  
 
 
 export { router }
