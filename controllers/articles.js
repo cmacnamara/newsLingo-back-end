28ed9789d4a6219853bd3333a4c9dcb996ffd0bc
Daniel Mathews <daniel.g.mathews@gmail.com>
2024-06-05 20:58:39 -0400

    refactor function for checking image URLs

--- controllers/articles.js
+++ controllers/articles.js
@@ -3,31 +3,31 @@ import { Profile } from "../models/profile.js"
 
 import axios from "axios"
 
-// async function create(req,res) {
-//   try {
-//     let filteredArticles =[]
-//     let nextPage=''
-//     for (let i=0; i<10; i++){
-//       const apiResponse = await fetch(`https://newsdata.io/api/1/news?apikey=${process.env.NEWS_API_KEY}&language=es${nextPage !== '' ? '&page=' + nextPage : ''}`)
-//       const articleData = await apiResponse.json()
-//       console.log("DATA", articleData);
+async function create(req,res) {
+  try {
+    let filteredArticles =[]
+    let nextPage=''
+    for (let i=0; i<10; i++){
+      const apiResponse = await fetch(`https://newsdata.io/api/1/news?apikey=${process.env.NEWS_API_KEY}&language=es${nextPage !== '' ? '&page=' + nextPage : ''}`)
+      const articleData = await apiResponse.json()
+      console.log("DATA", articleData)
 
-//       //Filter response with only articles that have required data attributes: creator, image, content
-//       const newArray= (articleData.results.filter(article => (
-//         article.creator && article.content)
-//       ))
-//       filteredArticles= [...filteredArticles, ...newArray]
-//       nextPage= articleData.nextPage
-//     }
+      //Filter response with only articles that have required data attributes: creator, image, content
+      const newArray= (articleData.results.filter(article => (
+        article.creator && article.content)
+      ))
+      filteredArticles= [...filteredArticles, ...newArray]
+      nextPage= articleData.nextPage
+    }
 
-//     const articles = await Article.create(filteredArticles)
-//     res.status(200).json(articles)
+    const articles = await Article.create(filteredArticles)
+    res.status(200).json(articles)
 
-//   } catch (error) {
-//     console.log(error);
-//     res.json(error)
-//   }
-// }
+  } catch (error) {
+    console.log(error);
+    res.json(error)
+  }
+}
 
 async function index(req,res) {
   try {
@@ -102,55 +102,48 @@ async function deleteComment(req, res) {
 }
 
 async function checkForValidImages(req, res) {
-  async function isValidImageUrl(url, articleId) {
-    try {
-      const response = await axios.head(url)
-      const contentType = response.headers['content-type']
-      return contentType.startsWith('image/')
-    } catch (err) {
-      // console.log("An error occurred while checking the URL:", err)
-      console.log("An error occurred while checking the URL:", articleId)
-      return false
-    }
-  }
+  let results = { validImages: 0, brokenImages: 0}
+  let invalidURLs = []
+  let nonImageURLs = []
   try {
-    //find all articles where imageURL is not null
     const articles = await Article.find({ image_url: { $ne: null }})
-      // .sort({ createdAt: 'desc' })
-      // res.status(200).json(articles)
-      // console.log(articles)
-    // const articleData = await articles.json()
-    //create a loop to send an http request to each imageURL
-    // articles.forEach(a => {
-    //   console.log(a.image_url)
-    // })
-
-    async function getStatusAndUpdateDB() {
-      //somehow we need our response to await the final tally of valid/invalid counts
-      // const responseObject
-      let validCount = 0
-      let invalidCount = 0
-      for(let article of articles) {
-        try {
-          const isValid = await isValidImageUrl(article.image_url, article._id)
-          isValid ? validCount++ : invalidCount++
-          console.log(`Article ID: ${article._id} contains a ${isValid ? 'valid' : 'broken'} image`)
-        } catch (err) {
-          // console.log("An error occurred while checking the URL: ", err)
-          console.log("An error occurred while checking the URL: ", article._id)
+    for(let article of articles) {
+      try {
+        const response = await axios.head(article.image_url)
+        const contentType = response.headers['content-type']
+        if(contentType.startsWith('image/')) {
+          console.log('this is a valid image')
+          results['validImages']++ 
+          console.log(results['validImages'])
+        } else {
+          console.log('this URL does not return an image')
+          results['brokenImages']++
+          nonImageURLs.push(article.image_url)
+          
         }
-        // const imgStatus = await fetch(article.image_url)
-        // console.log(imgStatus)
+      } catch (err) {
+        console.log('an error occured while checking that URL')
+        results['brokenImages']++
+        invalidURLs.push(article.image_url)
       }
-      return {validCount, invalidCount}
     }
-    getStatusAndUpdateDB()
-    res.status(200).json({validCount, invalidCount})
-    //
+    res.status(200).json({ results, invalidURLs, nonImageURLs})
   } catch (err) {
-    // console.log(err)
-    console.log('an error occurred')
+    console.log(err)
     res.status(500).json(err)
+  }
+}
+
+async function removeArticleURL(req, res) {
+  try {
+    const article = await Article.findById(req.params.articleId)
+    // console.log(article.image_url)
+    article.image_url = null
+    // console.log(`the image url is now ${article.image_url}`)
+    article.save()
+    res.status(200).json(article)
+  } catch (err) {
+    console.log(err)
   }
 }
 
@@ -162,6 +155,7 @@ export {
   updateComment,
   deleteComment,
   checkForValidImages,
+  removeArticleURL
 }
 
 //database has 1400 articles with full content up until 2023-11-22 (pubdate & createdAt date)
